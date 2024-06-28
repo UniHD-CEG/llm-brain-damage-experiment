91ebc45dde8ae47b4a2a5ead357744b850442176
Kevin Stehle <stehle@stud.uni-heidelberg.de>
2024-06-28 12:54:24 +0000

    Updated copyright notices

--- src/bitflipnoise.py
+++ src/bitflipnoise.py
@@ -1,5 +1,5 @@
-# Copyright (C) 2024 Yannick Emonds, Computing Systems Group, Institute of Computer Engineering, Heidelberg University.
-# Modifications cpyright (C) 2024 Computing Systems Group, Institute of Computer Engineering, Heidelberg University.
+# Copyright (C) 2024 Yannick Emonds
+# Modifications copyright (C) 2024 Franz Kevin Stehle Computing Systems Group, Institute of Computer Engineering, Heidelberg University.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
